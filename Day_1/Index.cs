ec01b074e5980e955e1fb8560bcf67cdf9aeb242
Higgins75 <danielmhiggins75@gmail.com>
2023-12-01 21:01:40 +0000

    No idea why this doesn't work tbh

---
+++ Day_1/Index.cs
@@ -0,0 +1,12 @@
+public class Index
+{
+    public bool checkFirst(int initialIndex, int CurrentIndex)
+    {
+        return CurrentIndex <= initialIndex;
+    }
+
+    public bool checkLast(int initialIndex, int CurrentIndex)
+    {
+        return CurrentIndex >= initialIndex;
+    }
+}
--- Day_1/stringParse.cs
+++ Day_1/stringParse.cs
@@ -1,38 +1,64 @@
 class stringParse
 {   
     Converters c = new Converters();
+    Index index = new Index();
     public int parseString(string testInput)
     {
         //initalise variables for use in for loop.
         int firstDigit = 0;
+        int firstDigitPosition = testInput.Length;
         int secondDigit = 0;
+        int secondDigitPosition = 0;
 
-        //bools to ensure system knows which number it is on
-        bool firstInput = true;
-        bool secondInput = false;
         
         for (int i = 0; i < testInput.Length; i++)
         {
             //init variable
             int testNum = c.ChartoInt(testInput[i]);
-
+            int digitPosition = i;
             //find the first number and save as firstDigit. Ensure future numbers do not override this one
-            if (c.isNumber(testNum) & firstInput == true)
+            if (c.isNumber(testNum) & index.checkFirst(firstDigitPosition, digitPosition))
             {
                 firstDigit = testNum;
-                firstInput = false;
+                firstDigitPosition = i;
+                Console.WriteLine("First digit set to {0} at {1}", firstDigit, firstDigitPosition);
             }
 
             //overrides the secondDigit variable if a number is found after the first was located
-            else if (c.isNumber(testNum) & firstInput == false)
+            if (c.isNumber(testNum) & index.checkLast(secondDigitPosition, digitPosition))
             {
                 secondDigit = testNum;
-                secondInput = true;
+                secondDigitPosition = i;
+                Console.WriteLine("Second digit set to {0} at {1}", secondDigit, secondDigitPosition);
             }
         }
 
+        string[] numberList = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+        for (int i = 0; i < numberList.Length; i++)
+        {
+            bool isPresent = testInput.Contains(numberList[i]);
+            if (isPresent)
+            {
+                int digitPosition = testInput.IndexOf(numberList[i]);
+                if (index.checkFirst(firstDigitPosition, digitPosition))
+                {
+                    firstDigit = i + 1;
+                    firstDigitPosition = digitPosition;
+                    Console.WriteLine("First digit set to {0} at {1}", firstDigit, firstDigitPosition);
+                }
+                if (index.checkLast(secondDigitPosition, digitPosition))
+                {
+                    secondDigit = i + 1;
+                    secondDigitPosition = digitPosition;
+                    Console.WriteLine("Second digit set to {0} at {1}", secondDigit, secondDigitPosition);
+                }
+            }
+        }
+
+
         //catches if the second digit did not appear
-        if (secondInput == false)
+        if (secondDigit == 0)
         {
             secondDigit = firstDigit;
         }
