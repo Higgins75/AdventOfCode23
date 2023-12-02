968bdfe2488c985b275e8afc2613d3de656308e8
Higgins75 <danielmhiggins75@gmail.com>
2023-12-02 13:17:06 +0000

    Part 1 solution complete

--- Day_2/Program.cs
+++ Day_2/Program.cs
@@ -11,17 +11,9 @@ class Program
         const int redCubesAllowed = 12;
         const int greenCubesAllowed = 13;
         const int blueCubesAllowed = 14;
-
         int GameSum = 0;
 
-
-        string [] ArrayList = new string[5];
-        ArrayList[0] = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
-        ArrayList[1] = "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue";
-        ArrayList[2] = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
-        ArrayList[3] = "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red";
-        ArrayList[4] = "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-        // string[] ArrayList = f.readFile("Day_2.txt");
+        string[] ArrayList = f.readFile("Day_2.txt");
 
         for (int i = 0; i < ArrayList.Length; i++)
         {
@@ -34,29 +26,27 @@ class Program
         
         foreach (var substring in cleanedString)
         {
-            // Console.WriteLine(substring);
             
-            redTotal += o.redCubes(substring);
-            // Console.WriteLine(redTotal);
+            if (o.redCubes(substring) > redTotal)
+            {
+                redTotal = o.redCubes(substring);
+            }
 
-            greenTotal += o.greenCubes(substring);
-            // Console.WriteLine(greenTotal);
+            if (o.greenCubes(substring) > greenTotal)
+            {
+                greenTotal = o.greenCubes(substring);
+            }
 
-            blueTotal += o.blueCubes(substring);
-            // Console.WriteLine(blueTotal);            
+            if (o.blueCubes(substring) > blueTotal)
+            {
+                blueTotal = o.blueCubes(substring);
+            }          
         }
-
-        // Console.WriteLine(redTotal);
-        // Console.WriteLine(greenTotal);
-        // Console.WriteLine(blueTotal);
 
         if ((redTotal <= redCubesAllowed) && (greenTotal <= greenCubesAllowed) && (blueTotal <= blueCubesAllowed))
         {
-            Console.WriteLine("Game ID {0} is valid", i + 1);
             GameSum += i + 1;
-            Console.WriteLine("GameSum is {0}", GameSum);
         }
-        else Console.WriteLine("Game ID {0} is not valid", i);
         }
 
         Console.WriteLine(GameSum);
