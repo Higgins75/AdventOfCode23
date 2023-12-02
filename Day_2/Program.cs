3c9f880b35c58f5a59116d633371c194564705ee
Higgins75 <danielmhiggins75@gmail.com>
2023-12-02 11:27:18 +0000

    Day 2 running now

--- Day_2/Program.cs
+++ Day_2/Program.cs
@@ -1,9 +1,13 @@
-﻿using System;
-
-class Program
+﻿class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello World");
+        const int redCubes = 12;
+        const int greenCubes = 13;
+        const int blueCubes = 14;
+
+        string testString = "Game 1: 3 blue, 2 green, 6 red; 17 green, 4 red, 8 blue; 2 red, 1 green, 10 blue; 1 blue, 5 green";
+
+        
     }
 }
