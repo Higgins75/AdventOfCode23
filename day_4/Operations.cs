eff6089d72ca78c6e9c13a120ef59898a063f5e3
Higgins75 <danielmhiggins75@gmail.com>
2023-12-04 18:38:22 +0000

    Started cleanup operations on input strings

---
+++ day_4/Operations.cs
@@ -0,0 +1,13 @@
+class Operations
+{
+    public string[] StringCleaner(string toClean)
+    {
+        string[] cleanedString = toClean.Split(':');
+        string[] stringArray = cleanedString[1].Split('|');
+        for (int i = 0; i < stringArray.Length; i++)
+        {
+            stringArray[i] = stringArray[i].Trim();
+        }
+        return stringArray;
+    }
+}
--- day_4/Program.cs
+++ day_4/Program.cs
@@ -2,6 +2,16 @@
 {
     static void Main()
     {
+        Operations o = new Operations();
         
+        string card = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
+
+        string[] splitCard = o.StringCleaner(card);
+
+        string winningNums = splitCard[0];
+        string personalNums = splitCard[1];
+
+        Console.WriteLine("winning numbers {0}, numbers I have {1}", winningNums, personalNums);
+
     }
 }
