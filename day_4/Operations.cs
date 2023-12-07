6aae2cef1b048a9d43b040f712cd87d8fa283525
Higgins75 <danielmhiggins75@gmail.com>
2023-12-07 21:06:27 +0000

    Commit for version parity

--- day_4/Operations.cs
+++ day_4/Operations.cs
@@ -10,4 +10,34 @@ class Operations
         }
         return stringArray;
     }
+
+    public List<int> GenerateNumberList(string card)
+    {
+        List<int> NumberList = new List<int>();
+        for (int i = 0; i < card.Length; i++)
+        {
+            if (isNum(card[i]))
+            {
+                //locationValues.Add(new LocationValues() {location = j, value = o.getASCIIValue(stringToTest[j])});
+                NumberList.Add(getASCIIValue(card[i]) * 10 + getASCIIValue[card[i + 1]]);
+                i++;
+            }
+        }
+        return NumberList;
+    }
+
+    public bool isNum(char test)
+    {
+        int value = Convert.ToInt32(test);
+        if (value > 47 && value < 58)
+        {
+            return true;
+        }
+        return false;
+    }
+    public int getASCIIValue(char test)
+    {
+        return Convert.ToInt32(test - 48);
+    }
+    
 }
--- day_4/Program.cs
+++ day_4/Program.cs
@@ -8,10 +8,17 @@
 
         string[] splitCard = o.StringCleaner(card);
 
-        string winningNums = splitCard[0];
-        string personalNums = splitCard[1];
+        // string winningNums = splitCard[0];
+        // string personalNums = splitCard[1];
+
+        list<int> winningNums = o.GenerateNumberList(splitCard[0]);
+        list<int> personalNums = o.GenerateNumberList(splitCard[1]);
 
         Console.WriteLine("winning numbers {0}, numbers I have {1}", winningNums, personalNums);
+
+        //foreach loop of personalNums against WinningNums. Count++ each time true. Using .Contains?
+
+
 
     }
 }
