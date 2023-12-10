5e0fbc45c92132fd2ac15b9da6c959e4c5905b4b
Higgins75 <danielmhiggins75@gmail.com>
2023-12-10 17:23:05 +0000

    First implementation to generate a list of seeds

---
+++ day_5/Operations.cs
@@ -0,0 +1,20 @@
+using System.Text.RegularExpressions;
+
+class Operations
+{
+        public List<int> GenerateNumberList(string input)
+    {
+        List<int> NumberList = new List<int>();
+        string pattern = @"\b\d+\b";
+        MatchCollection matches = Regex.Matches(input, pattern);
+
+        foreach (Match match in matches)
+        {
+            if (int.TryParse(match.Value, out int number))
+            {
+                NumberList.Add(number);
+            }
+        }
+        return NumberList;
+    }
+}
--- day_5/Program.cs
+++ day_5/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System.Runtime.CompilerServices;
+using System;
+using System.Runtime;
+
+class day_5_program
+{
+    static void Main()
+    {
+        fileReader f = new fileReader();
+        Operations o = new Operations();
+
+        string[] testString = f.readFile("day_5_test.txt");
+
+        // string seeds = "seeds:";
+        var seeds = new List<int>();
+
+        foreach (string item in testString)
+        {
+            if (item.Contains("seeds:"))
+            {
+                seeds.AddRange(o.GenerateNumberList(item));
+            }
+        }
+    }
+}
