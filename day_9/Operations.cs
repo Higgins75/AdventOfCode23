dc19af54f0717584049297b60256f4dbff3e44ce
Higgins75 <danielmhiggins75@gmail.com>
2023-12-17 22:10:25 +0000

    Day 9 part 1 complete

--- day_9/Operations.cs
+++ day_9/Operations.cs
@@ -5,7 +5,7 @@ class Operations
     public List<int> GenerateNumberList(string input)
     {
         List<int> NumberList = new List<int>();
-        string pattern = @"\b\d+\b";
+        string pattern = @"-?\b\d+\b";
         MatchCollection matches = Regex.Matches(input, pattern);
 
         foreach (Match match in matches)
@@ -16,5 +16,30 @@ class Operations
             }
         }
         return NumberList;
+    }
+
+    public List<int> extrapolate(List<int> input)
+    {
+        List<int> output = new List<int>();
+
+        for (int i = 0; i < input.Count - 1; i++)
+        {
+            output.Add(input[i + 1] - input [i]);
+        }
+
+        return output;
+    }
+
+    public bool CheckAllZeros(List<int> numbers)
+    {
+        foreach (int number in numbers)
+        {
+            if (number != 0)
+            {
+                return false; // Return false if any value is not equal to 0
+            }
+        }
+
+        return true; // If all values are equal to 0, return true
     }
 }
--- day_9/Program.cs
+++ day_9/Program.cs
@@ -1,4 +1,6 @@
-﻿class day_9_program
+﻿using System.Globalization;
+
+class day_9_program
 {
     static void Main()
     {
@@ -6,12 +8,31 @@
         Operations o = new Operations();
 
         string[] inputText = f.readFile("day_9.txt");
+        int extrapolatedSum = 0;
 
         foreach (string history in inputText)
         {
-            List<int> numbers = o.GenerateNumberList(history);
-
+            int extrapolatedValue = 0;
+            List<List<int>> numberLists = new List<List<int>>();
+            int count = 0;
             
+            numberLists.Add(o.GenerateNumberList(history));
+
+            while (!o.CheckAllZeros(o.extrapolate(numberLists[count])))
+            {
+                numberLists.Add(o.extrapolate(numberLists[count]));
+                count++;
+            }
+            numberLists.Add(o.extrapolate(numberLists[count]));            
+            
+            
+            for (int i = numberLists.Count - 1;  i > -1; i--)
+            {
+                List<int> listToCheck = numberLists[i];
+                extrapolatedValue += listToCheck[listToCheck.Count - 1];
+            }
+            extrapolatedSum += extrapolatedValue;
         }
+        Console.WriteLine(extrapolatedSum);
     }
 }
