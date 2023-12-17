1712b7fc0e5c02cad02c89dad6572ce5033ff845
Higgins75 <danielmhiggins75@gmail.com>
2023-12-17 16:33:15 +0000

    Works on test cases now

--- day_8/Program.cs
+++ day_8/Program.cs
@@ -7,7 +7,10 @@
 
         string[] inputFile = f.readFile("day_8.txt");
 
-        string Instructions = inputFile[0];
+        string instructions = inputFile[0];
+        string finalInputToLocate = "ZZZ";
+        bool inputLocated = false;
+        int count = 0;
 
         List<Map> mapList = new List<Map>();
 
@@ -15,5 +18,57 @@
         {
             mapList.Add(o.getMap(inputFile[i]));
         } 
+
+        Map mapToTest = mapList[0];
+
+        while (inputLocated == false)
+        {
+            foreach (char c in instructions)
+            {
+                switch (c)
+                {
+                    case 'R':
+                    if (mapToTest.rString == finalInputToLocate)
+                    {
+                        inputLocated = true;
+                        Console.WriteLine($"found ZZZ on R of {mapToTest.indexOfString}");
+                        count++;
+                        break;
+                    }
+                    foreach (var map in mapList)
+                    {
+                        if (map.indexOfString == mapToTest.rString)
+                        {
+                            mapToTest = map;
+                            count++;
+                            break;
+                        }
+                    }
+                    
+                    break;
+
+                    case 'L':
+                    if (mapToTest.lString == finalInputToLocate)
+                    {
+                        inputLocated = true;
+                        Console.WriteLine($"found ZZZ on L of {mapToTest.indexOfString}");
+                        count++;
+                        break;
+                    }
+                    foreach (var map in mapList)
+                    {
+                        if (map.indexOfString == mapToTest.lString)
+                        {
+                            mapToTest = map;
+                            count++;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine($"Found ZZZ at {mapToTest.indexOfString} in {count}");
     }
 }
