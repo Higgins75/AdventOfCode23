63718e5a868bfb1c64a160941d0286ce765ce997
Higgins75 <danielmhiggins75@gmail.com>
2023-12-19 17:38:52 +0000

    Added check functionality

--- day_10/Program.cs
+++ day_10/Program.cs
@@ -1,23 +1,44 @@
-﻿class day_10_program
+﻿using System.Collections;
+using System.Globalization;
+
+class day_10_program
 {
     static void Main()
     {
         fileReader f = new fileReader();
         string[] input = f.readFile("day_10.txt");
         Tuple<int, int> startingLocation = Tuple.Create(-1, -1);
-        int count = 0;
+        int stepsTaken = 0;
 
+        char[] East = {'-', 'J', '7'};
+        char[] South = {'|', 'J', 'L'};
+        char[] West = {'-', 'F', 'L'};
+        char[] North = {'|', 'F', '7'};
+
+        int Startcount = 0;
         foreach (string line in input)
         {
-            Console.WriteLine(line);
             if (line.Contains("F"))
             {
-                startingLocation = Tuple.Create(count, line.IndexOf("F"));
+                startingLocation = Tuple.Create(Startcount, line.IndexOf("S"));
+                break;
             }
-            count++;
-            
+            Startcount++;    
         }
 
-        Console.WriteLine(startingLocation.Item1 + " " + startingLocation.Item2);
+        
+
+
+
+        Console.WriteLine(input[1][1]);
+    }
+
+    public bool isComplete(Tuple<int, int> location, string[] input)
+    {
+        if (input[location.Item1][location.Item2] == 'S')
+        {
+            return true;
+        }
+        else return false;
     }
 }
