ec966acd9ef4e2534db21c4043fe2e641c4a1e7a
Higgins75 <danielmhiggins75@gmail.com>
2023-12-19 17:19:38 +0000

    Init location discovered

--- day_10/Program.cs
+++ day_10/Program.cs
@@ -2,6 +2,22 @@
 {
     static void Main()
     {
-        
+        fileReader f = new fileReader();
+        string[] input = f.readFile("day_10.txt");
+        Tuple<int, int> startingLocation = Tuple.Create(-1, -1);
+        int count = 0;
+
+        foreach (string line in input)
+        {
+            Console.WriteLine(line);
+            if (line.Contains("F"))
+            {
+                startingLocation = Tuple.Create(count, line.IndexOf("F"));
+            }
+            count++;
+            
+        }
+
+        Console.WriteLine(startingLocation.Item1 + " " + startingLocation.Item2);
     }
 }
