7d2b304c72604a834cd32c18525ee8312c2b3c76
Higgins75 <danielmhiggins75@gmail.com>
2023-12-26 18:48:44 +0000

    Updated code readability

--- day_13/Program.cs
+++ day_13/Program.cs
@@ -30,36 +30,21 @@ class day_13_program
 
     static string[] GetStringsFromColumns(string[] baseStrings)
     {
-        // Check if the array is empty or null
-        if (baseStrings == null || baseStrings.Length == 0)
-        {
-            throw new ArgumentException("The array of strings is empty or null.");
-        }
-
-        // Get the number of columns based on the length of the first string
         int numColumns = baseStrings[0].Length;
 
-        // Initialize a list to store the result strings
         List<string> result = new List<string>();
 
-        // Iterate through each column
         for (int col = 0; col < numColumns; col++)
         {
-            // StringBuilder to build the string for the current column
             StringBuilder columnStringBuilder = new StringBuilder();
 
-            // Iterate through each line and append the character at the current position to the column string
             for (int row = 0; row < baseStrings.Length; row++)
             {
-                // Check if the current line has enough characters
                 if (col < baseStrings[row].Length)
                 {
-                    // Append the character at the current position to the column string
                     columnStringBuilder.Append(baseStrings[row][col]);
                 }
             }
-
-            // Add the column string to the result list
             result.Add(columnStringBuilder.ToString());
         }
 
